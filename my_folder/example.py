b000a17deeefbedece57ed450251910ddd34ef35
montaga01 <montaga1salah@gmail.com>
2025-09-24 07:20:21 +0200

    Update main.py

--- my_folder/example.py
+++ my_folder/example.py
@@ -1,1 +1,19 @@
-# ملف تجريبي
+
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
+
+# توكن البوت من BotFather
+TOKEN = "8184537983:AAFmdN6BTH71K1jAqko46C7P5q9lFr3gldg"
+
+# دالة الرد على الرسائل
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+# دالة التشغيل الرئيسية
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
